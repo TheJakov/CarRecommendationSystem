d6c25ab81776f60b68102231adff281cc82b782a
TheJakov <kristovic.jakov@gmail.com>
2020-05-26 16:43:00 +0200

    Enabled car image display
    Enabled car image display  when prediction results are generated
    
    + Added car dataset image directory

--- CarRecommendationSystem/CarRecommendationSystem/Forms/Results.cs
+++ CarRecommendationSystem/CarRecommendationSystem/Forms/Results.cs
@@ -14,6 +14,7 @@ namespace CarRecommendationSystem.Forms
 {
     public partial class Results : Form
     {
+        private string imageDir = @"../../../../dataset_media/";
         public Results()
         {
             InitializeComponent();
@@ -39,6 +40,7 @@ namespace CarRecommendationSystem.Forms
             Alg1stLitres.Text = orderedList[0].Litres100km.ToString();
             Alg1stFuelType.Text = orderedList[0].FuelType;
             Alg1stTransmission.Text = orderedList[0].Transmission;
+            pictureBox1stAlg.ImageLocation = imageDir + orderedList[0].ID + ".png";
 
             //2nd
             Alg2ndCarName.Text = orderedList[1].Name + " - Score: " + orderedList[1].Score;
@@ -47,6 +49,7 @@ namespace CarRecommendationSystem.Forms
             Alg2ndLitres.Text = orderedList[1].Litres100km.ToString();
             Alg2ndFuelType.Text = orderedList[1].FuelType;
             Alg2ndTransmission.Text = orderedList[1].Transmission;
+            pictureBox2ndAlg.ImageLocation = imageDir + orderedList[1].ID + ".png";
 
             //3rd
             Alg3rdCarName.Text = orderedList[2].Name + " - Score: " + orderedList[2].Score;
@@ -55,6 +58,7 @@ namespace CarRecommendationSystem.Forms
             Alg3rdLitres.Text = orderedList[2].Litres100km.ToString();
             Alg3rdFuelType.Text = orderedList[2].FuelType;
             Alg3rdTransmission.Text = orderedList[2].Transmission;
+            pictureBox3rdAlg.ImageLocation = imageDir + orderedList[2].ID + ".png";
         }
     }
 }
