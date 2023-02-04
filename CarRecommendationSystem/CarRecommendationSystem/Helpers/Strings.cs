a6580b2f388ca6bce23702d4142bd3a5736da72b
TheJakov <kristovic.jakov@gmail.com>
2023-02-04 19:06:44 +0100

    Added UIHelper and Strings classes

---
+++ CarRecommendationSystem/CarRecommendationSystem/Helpers/Strings.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRecommendationSystem.Helpers
+{
+    public static class Strings
+    {
+        public static string Budget = "Budget";
+        public static string Horsepower = "Horsepower";
+        public static string YouNeedToCheckOneOption = "You need to check ONE option.";
+        public static string YouNeedToEnterIntegerValue = "You need to enter integer value.";
+        public static string Oops = "Oops!";
+    }
+}
---
+++ CarRecommendationSystem/CarRecommendationSystem/Helpers/UIHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CarRecommendationSystem.Helpers
+{
+    public static class UIHelper
+    {
+        public static void ShowInfoMessage(string text, string caption)
+        {
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
